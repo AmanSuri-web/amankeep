26d58e639af869ed16d60575628502eb24062e0c
aman suri <amansuri@MacBook-Pro.local>
2021-06-02 18:31:03 +0530

    make it better

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -8,7 +8,30 @@ import {Deletename,Deleteaddress,EditData,AddImage} from './EditUser';
 import Modal from 'react-bootstrap/Modal'
 import axios from 'axios'
 
+const Logout =()=>{
 
+  const {state,dispatch} = useContext(UserContext);
+  const history = useHistory();
+  
+    fetch('/logout',{
+          method:"GET",
+          headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json"
+            },
+            credentials: "include",
+      }).then((res)=>{
+          dispatch({type:"USER",payload:false})
+          history.push('/',{replace:true});
+          if (res.status !== 200) {
+              const error = new Error(res.error);
+              throw error;
+            }
+      }).catch((err)=>{
+          console.log(err);
+      })
+  
+}
 
 const About = () => {
   const history = useHistory();
@@ -153,6 +176,7 @@ const [useredit,setuseredit]=useState({
                 />
               <button onClick = {ChangeOnClick}> Upload</button> 
                 </div>
+                <button className="btn btn-outline-primary " style={{width:'100%',marginTop:'10px'}} onClick={Logout} >Logout</button>
                         </form>
                         
                 </div>
